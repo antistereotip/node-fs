da485c9f70310cb924a810a665f9eae3bb8c338c
Milutin Gavrilović <milutingavrilovic@yahoo.com>
2023-04-19 10:13:37 +0200

    Create ai1.py

---
+++ ai1.py
@@ -0,0 +1,30 @@
+import socket
+import random
+import time
+
+server = "localhost"
+port = 6667
+nickname = "Bot1"
+channel = "#test"
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc.connect((server, port))
+irc.send(("USER " + nickname + " " + nickname + " " + nickname + " :Python IRC\r\n").encode("UTF-8"))
+irc.send(("NICK " + nickname + "\r\n").encode("UTF-8"))
+irc.send(("JOIN " + channel + "\r\n").encode("UTF-8"))
+
+def send_message(message):
+    """Send a message to the IRC channel."""
+    irc.send(("PRIVMSG " + channel + " :" + message + "\r\n").encode("UTF-8"))
+
+while True:
+    message = irc.recv(2048).decode("UTF-8")
+    if message.find("PING") != -1:
+        irc.send(("PONG " + message.split()[1] + "\r\n").encode("UTF-8"))
+    if message.find(":!hello") != -1:
+        send_message("Hello there!")
+    if message.find(":!time") != -1:
+        current_time = time.strftime("%H:%M:%S", time.gmtime())
+        send_message("The current time is " + current_time)
+    if message.find(":!roll") != -1:
+        send_message("Rolling the dice...")
