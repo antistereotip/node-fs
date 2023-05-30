528582a7f49b1e1a39b9a82d521bc54d02e9a9f8
Milutin Gavrilović <milutingavrilovic@yahoo.com>
2023-05-30 20:21:01 +0200

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,8 +1,10 @@
 import socket
+import random
+import time
 
 server = "localhost"
 port = 6667
-nickname = "Bot"
+nickname = "Bot3"
 channel = "#test"
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,11 +13,20 @@ irc.send(("USER " + nickname + " " + nickname + " " + nickname + " :Python IRC\r
 irc.send(("NICK " + nickname + "\r\n").encode("UTF-8"))
 irc.send(("JOIN " + channel + "\r\n").encode("UTF-8"))
 
+def send_message(message):
+    """Send a message to the IRC channel."""
+    irc.send(("PRIVMSG " + channel + " :" + message + "\r\n").encode("UTF-8"))
+
 while True:
     message = irc.recv(2048).decode("UTF-8")
     if message.find("PING") != -1:
         irc.send(("PONG " + message.split()[1] + "\r\n").encode("UTF-8"))
     if message.find(":!hello") != -1:
-        irc.send(("PRIVMSG " + channel + " :Hello!\r\n").encode("UTF-8"))
-    if message.find(":!high") != -1:
-        irc.send(("PRIVMSG " + channel + " :Medvedima pristup zabranjen!\r\n").encode("UTF-8"))
+        send_message("Hello there!")
+    if message.find(":!time") != -1:
+        current_time = time.strftime("%H:%M:%S", time.gmtime())
+        send_message("The current time is " + current_time)
+    if message.find(":!roll") != -1:
+        send_message("Rolling the dice...")
+    if message.find(":!medved") != -1:
+        send_message("Medvedima pristup zabranjen...")
